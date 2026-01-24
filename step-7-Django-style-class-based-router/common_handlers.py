bf5d4f650a6c60c77a647bf9ba52d3eb932026d8
Mahi <anol.mahi@gmail.com>
2026-01-24 15:03:36 +0600

    :class-based-routing

---
+++ step-7-Django-style-class-based-router/common_handlers.py
@@ -0,0 +1,29 @@
+from contants import HttpStatus
+from helpers import json_response
+import logging
+from webob import Request,Response
+
+logger=logging.getLogger(__name__)
+
+class Handlers:
+    @staticmethod
+    def generic_exception_handler(request:Request,ex:Exception)->Response:
+        logger.exception(ex)
+        response={
+            "message": f" Unhandled exception occured {str(ex)}"
+        }
+        return Response(json_body=response,status=HttpStatus.INTERNAL_SERVER_ERROR)
+    
+    @staticmethod
+    def url_not_found_handler(request:Request)->Response:
+        response={
+            "message": f"Requested path:{request.path} does not exist"
+        }
+        return Response(json_body=response,status=HttpStatus.NOT_FOUND)
+    
+    @staticmethod
+    def method_not_allowed(request:Request,*args,**kwargs)->Response:
+        response={
+            "message": f"Requested method:{request.method} is not allowed for {request.path}"
+        }
+        return Response(json_body=response,status=HttpStatus.METHOD_NOT_ALLOWED)
