393ef99cdd36d253a1a50cd77ba4df406c423102
Mahi <anol.mahi@gmail.com>
2026-01-24 11:44:05 +0600

    :handling_path_var

---
+++ 5. Handling Path Variables/middlewares.py
@@ -0,0 +1,13 @@
+from webob import Request
+
+class ErrorHandlerMiddleware:
+    def __init__(self,app,ExceptionHandler:callable ):
+        self.wrapped_app=app
+        self.ExceptionHandler=ExceptionHandler
+    def __call__(self,envrion,start_response,*args,**kwds):
+        try:
+            return self.wrapped_app(envrion,start_response,*args,**kwds)
+        except Exception as e:
+            request=Request(envrion)
+            response=self.ExceptionHandler(request,e)
+            return response(envrion,start_response)
