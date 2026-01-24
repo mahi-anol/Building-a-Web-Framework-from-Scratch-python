d221f153ed1003032bf01ab409e8b3fa1359117c
Mahi <anol.mahi@gmail.com>
2026-01-24 15:42:47 +0600

    :refactoring

---
+++ step-8-Enhancement/middlewares.py
@@ -0,0 +1,14 @@
+from webob import Request
+
+class ErrorHandlerMiddleware:
+    def __init__(self,app,ExceptionHandler:callable ):
+        self.wrapped_app=app
+        self.ExceptionHandler=ExceptionHandler
+        print("Middleware is running...")
+    def __call__(self,envrion,start_response,*args,**kwds):
+        try:
+            return self.wrapped_app(envrion,start_response,*args,**kwds)
+        except Exception as e:
+            request=Request(envrion)
+            response=self.ExceptionHandler(request,e)
+            return response(envrion,start_response)
