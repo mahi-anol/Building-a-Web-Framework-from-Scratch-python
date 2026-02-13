2455aa9047aa5a5ad8a0449b779da4efc08dcd8b
mahi-anol <anol.mahi@gmail.com>
2026-02-14 01:46:46 +0600

    UNIT-test-added-14-2-26

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+from Framework import wsgi_framework
+import pytest
+from requests import Session as RequestSession
+from wsgiadapter import WSGIAdapter as RequestsWSGIAdapter
+from tests.constants import BASE_URL
+from Framework.middlewares import ErrorHandlerMiddleware
+class TFramework(wsgi_framework):
+    def test_session(self,base_url=BASE_URL):
+        session=RequestSession()
+        session.mount(prefix=base_url,adapter=RequestsWSGIAdapter(app=ErrorHandlerMiddleware(self)))
+        return session
+@pytest.fixture
+def app()->TFramework:
+    return TFramework()
+
+@pytest.fixture
+def client(app:TFramework):
+    return app.test_session()
+## mod 1
+# @pytest.fixture
+# def app()->wsgi_framework:
+#     return wsgi_framework()
+
---
+++ tests/test_function_based_handler.py
@@ -0,0 +1,65 @@
+from webob.response import Response
+from tests.constants import BASE_URL
+import pytest
+def test_client_and_send_request(app,client):
+    RESPONSE_TEXT="Hello from test client"
+    @app.route("/test")
+    def test_handler(req):
+        return Response(text=RESPONSE_TEXT)
+    
+    response=client.get(f"{BASE_URL}/test")
+    assert response.text==RESPONSE_TEXT
+
+
+@pytest.mark.parametrize(
+    "name, exp_result",
+    [
+        pytest.param(
+            'Mahi',"Hello Mahi",id="Mahi"
+        ),
+        pytest.param(
+            'Nafsi',"Hello Nafsi",id="Nafsi"
+        )
+    ]
+)
+def test_parameterized_route(app,client,name,exp_result):
+    @app.route('/hello/{name}')
+    def hello(req,name:str):
+        return Response(text=f"Hello {name}")
+    # Test multiple parameter values
+    assert client.get(f"{BASE_URL}/hello/{name}").text==exp_result
+
+
+def test_url_not_found(app,client):
+    RESPONSE_TEXT="Hello from test client"
+    @app.route("/test")
+    def test_handler(req):
+        return Response(text=RESPONSE_TEXT)
+
+    wrong_path="/hello"
+    exp_response={
+        "message":f"Request Path: {wrong_path} does not exist"
+    }
+    response=client.get(f"{BASE_URL}{wrong_path}")
+    assert response.status_code==404
+    assert response.json()==exp_response
+
+def test_generic_exception_handler(app,client):
+    msg="This is a test exception"
+    exp_response={"message":f"Unhandled Exception Occured: {msg}"}
+
+    @app.route("/test")
+    def test_handler(req):
+        raise RuntimeError(msg)
+    
+    response=client.get(f"{BASE_URL}/test")
+    assert response.status_code==500
+    assert response.json()==exp_response
+## mod 1
+# def test_parameterized_route(app,client):
+#     @app.route('/hello/{name}')
+#     def hello(req,name:str):
+#         return Response(text=f"Hello {name}")
+#     # Test multiple parameter values
+#     assert client.get(f"{BASE_URL}/hello/Mahi").text=="Hello Mahi"
+#     assert client.get(f"{BASE_URL}/hello/Nafsi").text=="Hello Nafsi"
