fa4f55cda5f735a37e77ca90f3ea1db5cf945097
Mahi <anol.mahi@gmail.com>
2026-02-15 01:03:33 +0600

    :middleware

--- Framework/middlewares.py
+++ Framework/middlewares.py
@@ -1,18 +1,54 @@
 from Framework.framework import wsgi_framework
 from Framework.command_handlers import CommonHandlers
-from webob import Request
-class ErrorHandlerMiddleware:
-    def __init__(self,
-                 app:wsgi_framework,
-                 exception_handler:callable=CommonHandlers.generic_exception_handler
-                ):
-        self.wrapped_app=app
-        self.exception_handler=exception_handler
+from webob import Request,Response
 
-    def __call__(self, environ,start_response):
+
+class Middleware:
+    def __init__(self,app:wsgi_framework):
+        self.app=app
+
+    def __call__(self, environ, start_response):
+        request=Request(environ)
+        response=self.handle_request(request)
+        return response(environ,start_response)
+    def add(self,middleware_cls)->None:
+        self.app=middleware_cls(app=self.app)
+
+    def process_request(self,req:Request)->None:
+        pass
+
+    def process_response(self,req:Request,resp:Response)->None:
+        pass
+
+    def handle_request(self,request:Request)->Response:
+        self.process_request(request)
+        response=self.app.handle_request(request)
+        self.process_response(request,response)
+        return response
+
+
+
+class ErrorHandlerMiddleware(Middleware):
+    def handle_request(self,request:Request)->Response:
         try:
-            return self.wrapped_app(environ,start_response)
+            return super().handler_request(request)
         except Exception as e:
-            request=Request(environ)
-            response=self.exception_handler(request,e)
-            return response(environ,start_response)
+            return CommonHandlers.generic_exception_handler(request,e)
+
+##mod1
+
+# class ErrorHandlerMiddleware:
+#     def __init__(self,
+#                  app:wsgi_framework,
+#                  exception_handler:callable=CommonHandlers.generic_exception_handler
+#                 ):
+#         self.wrapped_app=app
+#         self.exception_handler=exception_handler
+
+#     def __call__(self, environ,start_response):
+#         try:
+#             return self.wrapped_app(environ,start_response)
+#         except Exception as e:
+#             request=Request(environ)
+#             response=self.exception_handler(request,e)
+#             return response(environ,start_response)
