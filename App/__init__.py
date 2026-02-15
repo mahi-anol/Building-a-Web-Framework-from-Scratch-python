403fe16f9d93f227b730ebf8113cad7b9674076f
Mahi Sarwar Anol <anol.mahi@gmail.com>
2026-02-15 16:18:00 +0600

    :middle-ware-architecture

--- App/__init__.py
+++ App/__init__.py
@@ -1,11 +1,14 @@
 from Framework import wsgi_framework
-from Framework.middlewares import ErrorHandlerMiddleware
+from Framework.middlewares import ErrorHandlerMiddleware,ReqResLoggingMiddleware,ExecTimeMiddleware
 from pathlib import Path
 from Framework.command_handlers import CommonHandlers
-cwd=Path(__file__).resolve().parent
 
+cwd=Path(__file__).resolve().parent
 app=wsgi_framework(template_dir=f'{cwd}/templates',static_dir=f'{cwd}/static')
-app.add_exception_handler(handler=CommonHandlers.generic_exception_handler)
-exception_handler_middleware=ErrorHandlerMiddleware(
-    app=app
-)
+app.add_middleware(ReqResLoggingMiddleware)
+app.add_middleware(ExecTimeMiddleware)
+app.add_middleware(ErrorHandlerMiddleware)
+# app.add_exception_handler(handler=CommonHandlers.generic_exception_handler)
+# exception_handler_middleware=ErrorHandlerMiddleware(
+#     app=app
+# )
