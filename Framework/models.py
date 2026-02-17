16901637df00e94fa44a25c05f220e28cb714da9
Mahi Sarwar Anol <anol.mahi@gmail.com>
2026-02-17 10:18:13 +0600

    local changes before pull

--- Framework/models.py
+++ Framework/models.py
@@ -3,7 +3,7 @@ import inspect
 class RouteDefination:
     def __init__(self,handler,allowed_methods:Optional[list]=None,kwargs=None):
         self.handler=handler
-        self.allowed_methods=allowed_methods or ['GET',"POST","PUT","PATCH","DELETE"]
+        self.allowed_methods=allowed_methods or ["GET","POST","PUT","PATCH","DELETE"]
         self.kwargs=kwargs or {}
     def is_valid_methods(self,method:str)->bool:
         return method in self.allowed_methods
